505e867370d78613c03bac8e968b5ab7ac24bc04
pankaj-swami <kumarpankaj031093@gmail.com>
2018-09-20 14:53:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log("socket", io.engine.clientsCount)
+  socket.on('chat message', function(msg){
+  	console.log(msg)
+    io.emit('chat message', msg);
+  });
+});
+
+io.on('disconnect', function(user){
+	console.log("socket", io.engine.clientsCount)
+ 	io.emit('disconnect', user);
+ 	console.log(socket.name + ' has left the chat.');
+});
+
+
+
+http.listen(5000, function(){
+  console.log('listening on *:5000');
+});
